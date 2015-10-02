91f5b6fd76d9942774a4f53f13df21871a6ca8e9
susei <susei@sina.com>
2015-10-02 14:34:30 +0800

    vvvvvvvvvvvvvvv

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -1,13 +1,5 @@
 package com.example.myapp;
-<<<<<<< HEAD
 //xxx
-=======
-<<<<<<< HEAD
-/*xxxxxxxxxxx*/
-=======
-/*yyy*/
->>>>>>> dd220d372969859f6534a25f742f5c6c6add1ff8
->>>>>>> e243b88324da9de0681ebedeca03722560c2874c
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
