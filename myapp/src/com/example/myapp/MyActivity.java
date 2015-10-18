06f27ca86c3d5b03363a6f2d480bfe0291d5e189
susei <susei@sina.com>
2015-10-18 08:01:26 +0800

    测试回滚是什么意思

---
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+//xxx
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button bt = (Button) findViewById(R.id.bt);
+
+    }
+}
