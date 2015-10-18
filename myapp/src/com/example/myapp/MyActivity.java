2bb8e709d8ae9077caf369e6542a4ddb8fcc9488
susei <susei@sina.com>
2015-10-18 18:42:49 +0800

    新增信息

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -2,12 +2,13 @@
 * @Author: Marte
 * @Date:   2015-10-18 08:34:15
 * @Last Modified by:   Marte
-* @Last Modified time: 2015-10-18 08:56:18
+* @Last Modified time: 2015-10-18 18:42:11
 */
 
 public class java {
     public static void main(String[] args) {
         System.out.println("susei");
         System
+        QUQUQUQ 
     }
 }
