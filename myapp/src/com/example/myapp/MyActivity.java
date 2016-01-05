23c93d3ec854390306fc9d7ba84976cbc2531ac4
wb-suxiaohui <wb-suxiaohui@alibaba-inc.com>
2016-01-05 16:26:48 +0800

    1-5-1

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2015-10-18 08:34:15
 * @Last Modified by:   Marte
-* @Last Modified time: 2016-01-05 16:21:55
+* @Last Modified time: 2016-01-05 16:25:47
 */
 
 public class java {
@@ -11,5 +11,6 @@ public class java {
         System
         QUQUQUQ 
         1-5
+        1-5-1
     }
 }
