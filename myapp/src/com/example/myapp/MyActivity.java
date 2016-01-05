d95d56f30a60359cc743a79eeec6769f3cdc3393
wb-suxiaohui <wb-suxiaohui@alibaba-inc.com>
2016-01-05 16:24:37 +0800

    添加1-5

--- myapp/src/com/example/myapp/MyActivity.java
+++ myapp/src/com/example/myapp/MyActivity.java
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2015-10-18 08:34:15
 * @Last Modified by:   Marte
-* @Last Modified time: 2015-10-18 18:42:11
+* @Last Modified time: 2016-01-05 16:21:55
 */
 
 public class java {
@@ -10,5 +10,6 @@ public class java {
         System.out.println("susei");
         System
         QUQUQUQ 
+        1-5
     }
 }
